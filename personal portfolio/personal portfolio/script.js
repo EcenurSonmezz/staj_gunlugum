0caa690317d9ab14467d0ec17fe89fbb28251a9f
ece <115550511+EcenurSonmezz@users.noreply.github.com>
2023-08-23 22:30:57 +0300

    Add files via upload

---
+++ personal portfolio/personal portfolio/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('scroll load',function(){
+    $('#menu').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+  });
+
+  $('.portfolio .button-container .btn').click(function(){
+
+    let filter = $(this).attr('data-filter');
+
+    if(filter == 'all'){
+      $('.portfolio .image-container .box').show('400')
+    }else{
+      $('.portfolio .image-container .box').not('.'+filter).hide('200');
+      $('.portfolio .image-container .box').filter('.'+filter).show('400');
+    }
+
+  });
+
+  $('#theme-toggler').click(function(){
+    var button=$(this);
+
+    $(this).toggleClass('fa-sun');
+    setTimeout(function(){
+       $('body').toggleClass('dark-theme');
+    },300);
+   
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500,
+      'linear'
+    );
+
+  });
+
+  //send message
+
+  
+
+});
